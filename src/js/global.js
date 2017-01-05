2cdd8e894e16ada21d7069d09909991c485c43c8
Pavel Topol <khai603@gmail.com>
2017-01-05 15:55:22 +0200

    News page

--- src/js/global.js
+++ src/js/global.js
@@ -2,6 +2,7 @@
  * Third party
  */
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
 
 
 /*
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function($) {
-	
+	$('.nav-tabs > li').first().addClass('active');
+	$('.tab-content > *').first().addClass('active in');
+	$('.comments-more').on('click', function() {
+		var item = $('.comments-item'),
+			list = $('.comments-list'),
+			commentsCount = parseInt(item.length),
+			commentsShow = list.data('total'),
+			total = parseInt($('.comments-total').data('count'));
+
+		if(commentsCount >= total) {
+		    $(this).hide();
+			return false;
+		}
+
+		for(var i = 0; i < parseInt(total-commentsCount) && i < commentsShow; i++) {
+			list.append(item.first().clone());
+		}
+
+		if(parseInt(commentsCount + commentsShow) >= total) {
+		    $(this).hide();
+		}
+	});
 });
